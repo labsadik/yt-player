7fb18a4b7d8173370450c7aade8ebfd143c937dd
labsadik <jr.sadiklaskar7@gmail.com>
2026-04-04 18:51:19 +0530

    first commit

---
+++ list.js
@@ -0,0 +1,178 @@
+const videoList = [
+  {
+    no: 1,
+    id: "XCOZczc_Ljc",
+    title: "I Rise - Muhammad Al Muqit | Slowed and Reverb | NCN Release",
+    thumbnail: "https://img.youtube.com/vi/XCOZczc_Ljc/maxresdefault.jpg",
+    duration: "3:45"
+  },
+  {
+    no: 2,
+    id: "yVOCrDfJmMM",
+    title: "Relaxing Video",
+    thumbnail: "https://img.youtube.com/vi/yVOCrDfJmMM/hqdefault.jpg",
+    duration: "4:10"
+  },
+  {
+    no: 3,
+    id: "AE0legvgvig",
+    title: "Motivation Clip",
+    thumbnail: "https://img.youtube.com/vi/AE0legvgvig/hqdefault.jpg",
+    duration: "2:50"
+  },
+  {
+    no: 4,
+    id: "agayxv26R5o",
+    title: "Mohammed Al-Luhaidan Emotional Quran Recitation",
+    thumbnail: "https://img.youtube.com/vi/agayxv26R5o/hqdefault.jpg",
+    duration: "30:19"
+  },
+  {
+    no: 5,
+    id: "GfUbLz70dzw",
+    title: "Top Government Engineering Colleges In West Bengal 🏫 Best Engineering College In West Bengal",
+    thumbnail: "https://img.youtube.com/vi/GfUbLz70dzw/maxresdefault.jpg",
+    duration: "3:45"
+  },
+
+  {
+    no: 6,
+    id: "gHHXZ0b_obs",
+    title: "বনি ইসরাইলের গোত্র এবং ফেরাউন | আবু ত্বহা আদনান | সত্যের সন্ধান",
+    thumbnail: "https://img.youtube.com/vi/gHHXZ0b_obs/maxresdefault.jpg",
+    duration: ""
+  },
+  {
+    no: 7,
+    id: "P2c09xy7qig",
+    title: "Husain Huzaifa – أسمو (I Rise) | Cover | #محمد_المقيط",
+    thumbnail: "https://img.youtube.com/vi/P2c09xy7qig/maxresdefault.jpg",
+    duration: ""
+  },
+  {
+    no: 8,
+    id: "BB6bqyBR8Ok",
+    title: "ساعة كاملة | تلاوة القران بصوت عمير شميم للدراسة والتركيز",
+    thumbnail: "https://img.youtube.com/vi/BB6bqyBR8Ok/maxresdefault.jpg",
+    duration: "60:00"
+  },
+  {
+    no: 9,
+    id: "fo_XALcLLe4",
+    title: "إِنَّ اللَّهَ لَا يُضِيعُ أَجْرَ الْمُحْسِنِينَ",
+    thumbnail: "https://img.youtube.com/vi/fo_XALcLLe4/maxresdefault.jpg",
+    duration: ""
+  },
+  {
+    no: 10,
+    id: "5y8Z5goWD9U",
+    title: "Surah Yasin (Yaseen) | Soothing Quran Recitation for Calm Heart & Reflection",
+    thumbnail: "https://img.youtube.com/vi/5y8Z5goWD9U/maxresdefault.jpg",
+    duration: ""
+  },
+  {
+    no: 11,
+    id: "GI8FXcBWxOc",
+    title: "Complete Class 12th CHEMISTRY Basics in One Shot | Bridge Course HS 2027",
+    thumbnail: "https://img.youtube.com/vi/GI8FXcBWxOc/maxresdefault.jpg",
+    duration: "1:36:18"
+  },
+  {
+    no: 12,
+    id: "HHeb9MuRMjo",
+    title: "Bengali Stories for Kids | স্ট্রবেরি কাণ্ড | Bangla Cartoon",
+    thumbnail: "https://img.youtube.com/vi/HHeb9MuRMjo/maxresdefault.jpg",
+    duration: ""
+  },
+  {
+    no: 13,
+    id: "Q0eyAh-IUY0",
+    title: "Bengali Stories for Kids | গুলতির কেরামতি | Bangla Cartoon",
+    thumbnail: "https://img.youtube.com/vi/Q0eyAh-IUY0/maxresdefault.jpg",
+    duration: ""
+  },
+  {
+    no: 14,
+    id: "gUeQqOxuoJI",
+    title: "Bengali Stories for Kids | গাব্বু সিঙ্গার গ্রামে | Bangla Cartoon",
+    thumbnail: "https://img.youtube.com/vi/gUeQqOxuoJI/maxresdefault.jpg",
+    duration: ""
+  },
+  {
+    no: 15,
+    id: "RdJ-qW1eJSc",
+    title: "سورة الحجر - عثمان الحداد | Surah Al-Hijr Recitation",
+    thumbnail: "https://img.youtube.com/vi/RdJ-qW1eJSc/maxresdefault.jpg",
+    duration: ""
+  },
+  {
+    no: 16,
+    id: "1Hwmc2FnDE8",
+    title: "জ্ঞানী মানুষ কম কথা বলে | আবু ত্বহা মুহাম্মাদ আদনান",
+    thumbnail: "https://img.youtube.com/vi/1Hwmc2FnDE8/maxresdefault.jpg",
+    duration: ""
+  },
+  {
+    no: 17,
+    id: "oqjroNO6WZw",
+    title: "যা তোমার তা কখনো অন্য কারো হবেনা | আবু ত্বহা মুহাম্মদ আদনান",
+    thumbnail: "https://img.youtube.com/vi/oqjroNO6WZw/maxresdefault.jpg",
+    duration: ""
+  },
+  {
+    no: 18,
+    id: "wYGsGTyHmuw",
+    title: "أرح سمعك وقلبك بالقرآن | Mohamed Hesham Recitation",
+    thumbnail: "https://img.youtube.com/vi/wYGsGTyHmuw/maxresdefault.jpg",
+    duration: ""
+  },
+  {
+    no: 19,
+    id: "BsuPmtcNB74",
+    title: "Stop Coding Like a Follower! 5 Mistakes Keeping You Stuck",
+    thumbnail: "https://img.youtube.com/vi/BsuPmtcNB74/maxresdefault.jpg",
+    duration: "0:05+"
+  },
+  {
+    no: 20,
+    id: "9uWBy6f97Oo",
+    title: "Why 90% of Coders Never Become Developers",
+    thumbnail: "https://img.youtube.com/vi/9uWBy6f97Oo/maxresdefault.jpg",
+    duration: ""
+  },
+  {
+    no: 21,
+    id: "Uf6PXnagtsg",
+    title: "Complete Docker + AWS | Deploy Your First Project in One Shot",
+    thumbnail: "https://img.youtube.com/vi/Uf6PXnagtsg/maxresdefault.jpg",
+    duration: ""
+  },
+  {
+    no: 22,
+    id: "pkKn8q5AvsY",
+    title: "Complete Authentication System | JWT, Refresh Token, OTP",
+    thumbnail: "https://img.youtube.com/vi/pkKn8q5AvsY/maxresdefault.jpg",
+    duration: "25:03+"
+  },
+  {
+    no: 23,
+    id: "zG3hNL08Dro",
+    title: "Gen AI + Full Stack Web Development Project | React + Node + Gemini",
+    thumbnail: "https://img.youtube.com/vi/zG3hNL08Dro/maxresdefault.jpg",
+    duration: ""
+  },
+  {
+    no: 24,
+    id: "tlAsw1pbM4U",
+    title: "Complete Class 12th MATHS Basics in One Shot | Bridge Course HS 2027",
+    thumbnail: "https://img.youtube.com/vi/tlAsw1pbM4U/maxresdefault.jpg",
+    duration: ""
+  },
+  {
+    no: 25,
+    id: "2K7zUjC3Yt8",
+    title: "PHYSICAL WORLD AND MEASUREMENT | WBJEE Crash Course Physics FREE Class",
+    thumbnail: "https://img.youtube.com/vi/2K7zUjC3Yt8/maxresdefault.jpg",
+    duration: ""
+  }
+];
